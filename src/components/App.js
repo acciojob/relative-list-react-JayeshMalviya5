e45a5a893a2b26ae52d349779c8367517c48b5f5
jayeshmalviya5 <jayeshmalviya10@gmail.com>
2023-02-13 23:22:31 +0530

    dbckj

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
 const App = () => {
+  const relativeList = [
+    { key: 'relativeListItem1', name: 'Aunt Mary' },
+    { key: 'relativeListItem2', name: 'Uncle John' },
+    { key: 'relativeListItem3', name: 'Grandma' },
+    { key: 'relativeListItem4', name: 'Grandpa' }
+  ];
+  
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+<ol>
+      {relativeList.map(relative => (
+        <li key={relative.key}>{relative.name}</li>
+      ))}
+    </ol>
     </div>
+    
   )
 }
 
