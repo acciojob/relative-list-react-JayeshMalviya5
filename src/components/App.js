dbd3c38b7b786f2a9f1758e19294d77332504864
jayeshmalviya5 <jayeshmalviya10@gmail.com>
2023-02-14 11:18:38 +0530

    cgghg

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div id="main">
       <div>
-        <ol key="relativeList">
+        <ol id="relativeList">
           {relativeList.map((relative) => (
             <li id={relative.key}>{relative.name}</li>
           ))}
