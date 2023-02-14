87d4136961657aa5c9debcc6a1bae9390b59b77e
jayeshmalviya5 <jayeshmalviya10@gmail.com>
2023-02-14 11:16:34 +0530

    txtrfgrc

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   const relativeList = [
-    { key: 'relativeListItem1', name: 'Aunt Mary' },
-    { key: 'relativeListItem2', name: 'Uncle John' },
-    { key: 'relativeListItem3', name: 'Grandma' },
-    { key: 'relativeListItem4', name: 'Grandpa' }
+    { key: "relativeListItem1", name: "Aunt Mary" },
+    { key: "relativeListItem2", name: "Uncle John" },
+    { key: "relativeListItem3", name: "Grandma" },
+    { key: "relativeListItem4", name: "Grandpa" },
   ];
-  
+
   return (
     <div id="main">
- <ol key="relativeList">
-    {relativeList.map(relative => (
-      <li key={relative.key}>{relative.name}</li>
-    ))}
-  </ol>
+      <div>
+        <ol key="relativeList">
+          {relativeList.map((relative) => (
+            <li key={relative.key}>{relative.name}</li>
+          ))}
+        </ol>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
