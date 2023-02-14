6644647f3b2f45af0cca8caa5d7792f7dc698200
jayeshmalviya5 <jayeshmalviya10@gmail.com>
2023-02-14 11:18:15 +0530

    uubhbhb

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <div>
         <ol key="relativeList">
           {relativeList.map((relative) => (
-            <li key={relative.key}>{relative.name}</li>
+            <li id={relative.key}>{relative.name}</li>
           ))}
         </ol>
       </div>
